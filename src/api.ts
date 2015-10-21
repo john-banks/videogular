c77a8c4e6e3274e26163058d988ecdbb2d9c2ff4
Bernhelm <cernhelm@gmail.com>
2015-10-21 22:27:17 +0200

    Minor fixes to d.ts

--- src/api.ts
+++ src/api.ts
@@ -1,2 +1,3 @@
 export * from './com/2fdevs/videogular/services/vg-api';
+export * from './com/2fdevs/videogular/events/VgEvents';
 export * from './com/2fdevs/videogular/services/vg-fullscreen-api';
--- src/videogular2.ts
+++ src/videogular2.ts
@@ -1,4 +1,3 @@
-export * from './core';
-export * from './profile';
-export * from './lifecycle_hooks';
-export * from './bootstrap';
+export * from './api';
+export * from './player';
+export * from './plugins';
